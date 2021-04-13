9ffbc3bba941e307ce0f16c9e69343cfd6c0db23
SeungWookHan <hswook12@me.com>
2021-04-13 13:09:01 +0900

    Add: Grid

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles({
   card: {
@@ -34,21 +35,25 @@ function App() {
         <Typography color="textPrimary" gutterBottom variant="h2" align="center">
           React Material UI Example
         </Typography>
-          {
-            data.map((character) => (
-              <Card className={classes.card}>
-                <CardMedia className={classes.media} image={character.img_url} />
-                  <CardContent>
-                    <Typography color="primary" variant="h5">
-                      {character.name}
-                    </Typography>
-                    <Typography color="textSecondary" variant="subtitle2">
-                      {character.status}
-                    </Typography>
-                  </CardContent>
-              </Card>
-            ))
-          }
+        <Grid container spacing={3}>
+            {
+              data.map((character) => (
+                <Grid item xs={12} sm={4} key={character.id}>
+                  <Card className={classes.card}>
+                    <CardMedia className={classes.media} image={character.img_url} />
+                      <CardContent>
+                        <Typography color="primary" variant="h5">
+                          {character.name}
+                        </Typography>
+                        <Typography color="textSecondary" variant="subtitle2">
+                          {character.status}
+                        </Typography>
+                      </CardContent>
+                  </Card>
+                </Grid>
+              ))
+            }
+          </Grid>
       </Container>
     </div>
   );
